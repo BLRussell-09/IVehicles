25f1aa84cb9923c83a22da2ddec5552a597e8da5
Barry Russell <blrussell09@gmail.com>
2018-09-15 09:21:53 -0500

    Add project files.

---
+++ VehicleInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehicleInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
